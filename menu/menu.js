b3662201f739971ad2e62000911a1fcc74306aff
Jonathan Felicity <jonathanfelicity@mail.com>
2023-01-30 19:41:34 +0100

    first commit

---
+++ menu/menu.js
@@ -0,0 +1,16 @@
+
+
+const { Menu, Shell } = require('electron')
+
+
+const template = [
+    // {
+    //     role: 'About'
+    // }
+]
+
+
+const menu = Menu.buildFromTemplate(template)
+
+
+module.exports = menu
